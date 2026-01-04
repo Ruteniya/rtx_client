7f691fc4cd3b633e230d3b22cd3a6f6cc3548ef5
Mariia Padalka <ms.padalka@gmail.com>
2026-01-04 21:44:13 +0200

    add api to the api slice

--- src/api/api-slice.ts
+++ src/api/api-slice.ts
@@ -2,7 +2,7 @@ import { baseQueryWithReauth } from '@app/base-query-with-reauth'
 import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-  reducerPath: '',
+  reducerPath: 'api',
   baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
   tagTypes: [
