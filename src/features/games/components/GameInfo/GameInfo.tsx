caaf0885a809d00d25e53d0d81da081141ce8443
Mariia Padalka <ms.padalka@gmail.com>
2026-02-06 00:43:34 +0200

    Enhance GameInfo component to display game status with color-coded tags; update game description rendering for improved HTML support.

--- src/features/games/components/GameInfo/GameInfo.tsx
+++ src/features/games/components/GameInfo/GameInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Flex } from 'antd'
+import { Typography, Flex, Tag } from 'antd'
 import { Pto } from 'rtxtypes'
 import { GameLogo } from '..'
 
@@ -10,18 +10,20 @@ interface GameInfoProps {
 const GameInfo: React.FC<GameInfoProps> = ({ game }) => {
 
   const gameStatusOptions = [
-    { value: Pto.Games.GameStatus.Draft, label: 'В процесі розробки' },
-    { value: Pto.Games.GameStatus.Running, label: 'Активна' },
-    { value: Pto.Games.GameStatus.Stopped, label: 'Зупинена' },
-    { value: Pto.Games.GameStatus.Finished, label: 'Завершена' }
+    { value: Pto.Games.GameStatus.Draft, label: 'В процесі розробки' , color: 'orange'},
+    { value: Pto.Games.GameStatus.Running, label: 'Активна' , color: 'green'},
+    { value: Pto.Games.GameStatus.Stopped, label: 'Зупинена' , color: 'red'},
+    { value: Pto.Games.GameStatus.Finished, label: 'Завершена' , color: 'blue'},
   ]
 
   return (
     <Flex vertical justify="center" align="center" className="text-center items-center w-full">
       <GameLogo logo={game?.logo || ''} />
       <Typography.Title>{game.name}</Typography.Title>
-      <Typography.Paragraph>{game.description}</Typography.Paragraph>
-      <Typography.Paragraph>Статус: {gameStatusOptions.find((status) => status.value === game.status)?.label}</Typography.Paragraph>
+      <Typography.Paragraph>
+      <span dangerouslySetInnerHTML={{ __html: game.description }} />
+    </Typography.Paragraph>
+      <Typography.Paragraph><strong>Статус:</strong> <Tag color={gameStatusOptions.find((status) => status.value === game.status)?.color}>{gameStatusOptions.find((status) => status.value === game.status)?.label}</Tag></Typography.Paragraph>
       <strong>Дата початку:</strong>{' '}
       {new Date(game.startDate).toLocaleString('uk-UA', {
         year: 'numeric',
